2b591c10ccfdd8be6932f39fcbedba3e7b85e86a
Виталий Семенюк <kilativs@rambler.ru>
2021-10-25 21:13:12 +0300

    Initial commit

---
+++ PortScaner/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace PortScaner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IEnumerable<Port> port = MainWindow.GetOpenPort();
+            ListviewScaner.ItemsSource = port;
+        }
+
+        private static IEnumerable<Port> GetOpenPort()
+        {
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+            properties.GetActiveTcpListeners();
+            TcpConnectionInformation[] tcpConnections = properties.GetActiveTcpConnections();
+
+            return tcpConnections.Select(p => new Port(
+                p.LocalEndPoint.Port,
+                $"{p.LocalEndPoint.Address}:{p.LocalEndPoint.Port}",
+                $"{p.RemoteEndPoint.Address}:{p.RemoteEndPoint.Port}",
+                p.State.ToString())).ToList();
+        }
+    }
+}
---
+++ PortScaner/Port.cs
@@ -0,0 +1,18 @@
+﻿namespace PortScaner
+{
+    internal class Port
+    {
+        public int PortNumber { get; set; }
+        public string LocalAdress { get; set; }
+        public string RemoteAdress { get; set; }
+        public string State { get; set; }
+
+        public Port(int portNumber, string local, string remote, string state)
+        {
+            PortNumber = portNumber;
+            LocalAdress = local;
+            RemoteAdress = remote;
+            State = state;
+        }
+    }
+}
